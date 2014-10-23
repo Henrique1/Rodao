b6387a85c75b67a0b507bcdfbf24863cbe669335
Henrique <henrique.balbino.silva@hotmail.com>
2014-10-23 11:58:14 -0200

    Fazendo a lista de pedido

---
+++ Quiron.LojaVirtual.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiron.LojaVirtual.Dominio.Entidades
+{
+    public class Pedido
+    {
+        [Required(ErrorMessage = "Informe seu Nome!")]
+        public string NomeCliente { get; set; }
+
+
+        [Display(Name = "Cep:")]
+        public string Cep { get; set; }
+
+
+        [Required(ErrorMessage = "Informe seu Endereço!")]
+        [Display(Name = "Endereço:")]
+        public string Endereco { get; set; }
+
+
+        [Display(Name = "Complemento:")]
+        public string Complemento { get; set; }
+
+
+        [Required(ErrorMessage = "Informe sua Cidade!")]
+        [Display(Name = "Cidade:")]
+        public string Cidade { get; set; }
+
+
+        [Required(ErrorMessage = "Informe seu Bairro!")]
+        [Display(Name = "Bairro:")]
+        public string Bairro { get; set; }
+
+
+        [Required(ErrorMessage = "Informe seu E-mail!")]
+        [Display(Name = "E-mail:")]
+        [EmailAddress(ErrorMessage = "E-mail inválido")]
+        public string Email { get; set; }
+
+        public bool EmbrulhaPresente { get; set; }
+
+
+
+    }
+}
